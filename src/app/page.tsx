348d56690b572ffb4873b87984945c23eed4f077
David Le Marchand <DLM16091962@gmail.com>
2025-10-20 09:26:52 +0000

    Code is still incorrect! - Current DC Pension Value and Initial DC Pensi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 import React, { useState, useMemo, useRef, useEffect, useCallback } from 'react';
 import { useForm, Controller, type SubmitHandler } from "react-hook-form";
@@ -661,6 +660,8 @@ export default function PensionPilotPage() {
     </div>
   );
 }
+
+    
 
     
 
